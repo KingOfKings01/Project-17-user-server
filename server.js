046379541e36053d9d82aa709da7733f79b06b92
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-29 01:44:38 +0530

    Create a start up:
    Express set
    Prisma set with postgre sql and write the schema.
    
    Create necessary files and folders.
    
    Setup server and tested.

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// const categoryRoutes = require("./routes/categoryRoutes");
+// const movieRoutes = require("./routes/movieRoutes");
+// const showtimeRoutes = require("./routes/showtimeRoutes");
+// const bookedRoutes = require("./routes/bookedRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+// app.use("/categories", categoryRoutes);
+// app.use("/movies", movieRoutes);
+// app.use("/showtime", showtimeRoutes);
+// app.use("/booked", bookedRoutes);
+
+// Basic route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the User Server" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
